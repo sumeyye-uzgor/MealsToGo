9e512af6c381fd8ab0cf60dcce5285820c9fed71
Sumeyye <summeyyeoz@gmail.com>
2021-07-20 00:57:10 +0300

    small fix

--- src/features/restourants/screens/Restourants.screen.js
+++ src/features/restourants/screens/Restourants.screen.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { Platform, StyleSheet, Text, View, StatusBar } from "react-native";
+import { SafeAreaView, Text, View, StatusBar } from "react-native";
 import styled from "styled-components/native";
 import RestourantInfoCard from "../components/RestourantInfoCard.component";
-const StyledView = styled.SafeAreaView`
+const StyledView = styled(SafeAreaView)`
   flex: 1;
   margin-top: ${Math.ceil(StatusBar.currentHeight)}px;
 `;
-const StyledSearchView = styled.View`
+const StyledSearchView = styled(View)`
   padding: 16px;
 `;
-const StyledContainer = styled.View`
+const StyledContainer = styled(View)`
   flex: 1;
   padding: 16px;
 `;
