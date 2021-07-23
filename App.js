de9398b895b22ff1233de81f4eb6a54c412d64bc
Sumeyye <summeyyeoz@gmail.com>
2021-07-23 19:28:35 +0300

    theme addition and increasing consistency

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import RestaurantsScreen from "./src/features/restourants/screens/Restaurants.screen";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/infrastructures/theme";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({ Oswald_400Regular });
+  const [latoLoaded] = useLato({ Lato_400Regular });
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar />
     </>
   );
